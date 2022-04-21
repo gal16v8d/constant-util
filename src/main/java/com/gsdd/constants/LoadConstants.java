4352f20c8b83e64757f1c2cd88195e404972f9b9
Alex Galvis <alex.galvis.sistemas@gmail.com>
2022-04-20 21:30:34 -0500

    Replace formatter by spotless plugin

--- src/main/java/com/gsdd/constants/LoadConstants.java
+++ src/main/java/com/gsdd/constants/LoadConstants.java
@@ -6,12 +6,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class LoadConstants {
 
-  public static final String IMPORT = "/sql/import.sql";
-  public static final String GSDD_HOME = "GSDD_HOME";
-  public static final String GRAL_PROPERTIES = "/gsdd.properties";
+    public static final String IMPORT = "/sql/import.sql";
+    public static final String GSDD_HOME = "GSDD_HOME";
+    public static final String GRAL_PROPERTIES = "/gsdd.properties";
 
-  public static String getHome() {
-    return System.getenv(GSDD_HOME);
-  }
-
+    public static String getHome() {
+        return System.getenv(GSDD_HOME);
+    }
 }
